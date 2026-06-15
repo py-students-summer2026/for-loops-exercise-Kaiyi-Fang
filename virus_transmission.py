4f620b6452a436a602e0eb2f5595a25fd29e6be5
Kaiyi Fang <kaiyif7@gmail.com>
2026-06-15 21:57:30 +0800

    Completed for loop exercise

--- virus_transmission.py
+++ virus_transmission.py
@@ -17,3 +17,9 @@ def calculate_infections(starting_number_infections, reproduction_rate, num_days
     :param reproduction_rate: A floating point number indicating the rate of growth in the number of infections each day.
     :returns: The new number of infected individuals after the given number of days, rounded to the nearest integer.
     """
+    number_infected = starting_number_infections
+
+    for day in range(num_days):
+        number_infected = number_infected * reproduction_rate
+    number_infected = round(number_infected)
+    return number_infected
